ae34837108ae5c5d0328940c67783ef4493a1229
Akshoo <akshayshukla197@gmail.com>
2024-07-07 02:40:21 +0530

    removed fractional module added fraction.js module

--- src/js/views/RecipeView.js
+++ src/js/views/RecipeView.js
@@ -1,6 +1,6 @@
 import View from './View';
 import icons from '../../img/icons.svg';
-import 'fractional';
+import Fraction from 'fraction.js';
 
 class RecipeView extends View {
 	// // this._data is{
@@ -139,7 +139,7 @@ class RecipeView extends View {
               <use href="${icons}#icon-check"></use>
             </svg>
             <div class="recipe__quantity">${
-				ing.quantity == null ? '' : new Fraction(ing.quantity).toString()
+				ing.quantity == null ? '' : new Fraction(ing.quantity).toFraction(true)
 			}</div>
             <div class="recipe__description">
               <span class="recipe__unit">${ing.unit}</span>
